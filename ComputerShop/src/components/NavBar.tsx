36adc3e76f4b98ea1e56bda14d22acb3c7dc1e10
pitzee <pitzee@gmail.com>
2024-09-15 14:11:42 +0300

    search added

--- ComputerShop/src/components/NavBar.tsx
+++ ComputerShop/src/components/NavBar.tsx
@@ -3,11 +3,23 @@ import computer from "../assets/images/laptop.png";
 const NavBar = () => {
   return (
     <>
+      {/* main div */}
+
       <div className="flex flex-col md:flex-row md:justify-between bg-slate-200 ">
-        <a href="">
+        {/* home icon */}
+
+        <a href="" className="pl-2">
           <img src={computer} alt="" className="w-10 h-10" />
         </a>
-        <nav className="flex md:space-x-3">
+        {/* search  */}
+        <div>
+          <input type="text" className="p-2 rounded-l-md border-2" />
+          <button className="rounded-r-md bg-green-500 p-2 ">Search</button>
+        </div>
+
+        {/* menu items */}
+
+        <nav className="flex md:space-x-3 items-center pr-2">
           <a href="#" className="text-lg font-bold  hover:text-purple-500">
             HOME
           </a>
