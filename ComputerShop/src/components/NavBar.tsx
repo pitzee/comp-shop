91d3cdc47efe8c6841a8718c971718a90fba06d7
pitzee <pitzee@gmail.com>
2024-09-15 13:41:09 +0300

    home icon added

--- ComputerShop/src/components/NavBar.tsx
+++ ComputerShop/src/components/NavBar.tsx
@@ -1,19 +1,26 @@
+import computer from "../assets/images/laptop.png";
+
 const NavBar = () => {
   return (
     <>
-      <div className="flex flex-col  md:flex-row md:justify-end p-2 md:space-x-3 bg-slate-200">
-        <a href="#" className="text-lg font-bold hover:text-purple-500">
-          HOME
+      <div className="flex flex-col md:flex-row md:justify-between bg-slate-200 ">
+        <a href="">
+          <img src={computer} alt="" className="w-10 h-10" />
         </a>
-        <a href="#" className="text-lg font-bold hover:text-purple-500">
-          Products
-        </a>
-        <a href="#" className="text-lg font-bold hover:text-purple-500">
-          Aboutus
-        </a>
-        <a href="#" className="text-lg font-bold hover:text-purple-500">
-          Contact Us
-        </a>
+        <nav className="flex md:space-x-3">
+          <a href="#" className="text-lg font-bold  hover:text-purple-500">
+            HOME
+          </a>
+          <a href="#" className="text-lg font-bold hover:text-purple-500">
+            Products
+          </a>
+          <a href="#" className="text-lg font-bold hover:text-purple-500">
+            Aboutus
+          </a>
+          <a href="#" className="text-lg font-bold hover:text-purple-500">
+            Contact Us
+          </a>
+        </nav>
       </div>
     </>
   );
