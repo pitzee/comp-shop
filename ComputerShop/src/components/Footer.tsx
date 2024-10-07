82d0f98974c9c5b0e9ef518024ac1b242015da53
pitzee <pitzee@gmail.com>
2024-10-07 21:10:00 +0300

    validation added to the email subscription

--- ComputerShop/src/components/Footer.tsx
+++ ComputerShop/src/components/Footer.tsx
@@ -1,10 +1,27 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
 interface props {
   onSubscribe: () => void;
   email: string;
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
+const schema = z.object({
+  email: z.string().email({ message: "Inavlid email address" }),
+});
+
 const Footer = ({ onSubscribe, email, onChange }: props) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(schema),
+  });
+
   return (
     <footer className="bg-slate-200">
       <div className="mt-4 p-3 flex justify-between border border-b-2 border-b-gray-300 ">
@@ -19,14 +36,18 @@ const Footer = ({ onSubscribe, email, onChange }: props) => {
             className="p-2 border-2 rounded-l-md"
             placeholder="Email address"
             value={email}
+            {...register("email")}
             onChange={onChange}
           />
           <button
             className=" pl-2 border  p-2 bg-green-600"
-            onClick={onSubscribe}
+            onClick={handleSubmit(onSubscribe)}
           >
             Subscribe
           </button>
+          {errors.email && (
+            <p className="text-red-500">{errors.email.message as string}</p>
+          )}
         </div>
       </div>
       <div className="flex justify-between p-2">
