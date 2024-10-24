fd0e586a8dba590b433575845865af43dcd07f88
pitzee <pitzee@gmail.com>
2024-10-24 19:46:33 +0300

    carosel updated

--- ComputerShop/src/components/Home.tsx
+++ ComputerShop/src/components/Home.tsx
@@ -1,117 +1,21 @@
-import { useState, useEffect } from "react";
+import { Carousel } from "flowbite-react";
+("use client");
+
 import pc1 from "../assets/images/pc1.jpg";
 import pc2 from "../assets/images/pc2.jpg";
 import pc3 from "../assets/images/pc3.jpg";
 import pc4 from "../assets/images/pc4.jpg";
 
 const Home = () => {
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const slides = [pc1, pc2, pc3, pc4];
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentIndex((prevIndex) => (prevIndex + 1) % slides.length);
-    }, 3000); // Change slide every 3 seconds
-    return () => clearInterval(interval);
-  }, [slides.length]);
-
   return (
     <>
-      <div
-        id="default-carousel"
-        className="relative w-full"
-        data-carousel="slide"
-      >
-        <div className="mt-14 relative h-56 overflow-hidden rounded-lg md:h-96 bg-slate-300">
-          {slides.map((slide, index) => (
-            <div
-              key={index}
-              className={`duration-700 ease-in-out ${
-                currentIndex === index ? "block" : "hidden"
-              }`}
-              data-carousel-item
-            >
-              <img
-                src={slide}
-                className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-                alt={`Slide ${index + 1}`}
-              />
-            </div>
-          ))}
-        </div>
-
-        <div className="absolute z-30 flex -translate-x-1/2 bottom-5 left-1/2 space-x-3 rtl:space-x-reverse">
-          {slides.map((_, index) => (
-            <button
-              key={index}
-              type="button"
-              className={`w-3 h-3 rounded-full ${
-                currentIndex === index ? "bg-blue-600" : "bg-white"
-              }`}
-              aria-current={currentIndex === index ? "true" : "false"}
-              aria-label={`Slide ${index + 1}`}
-              data-carousel-slide-to={index}
-              onClick={() => setCurrentIndex(index)}
-            ></button>
-          ))}
-        </div>
-
-        <button
-          type="button"
-          className="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
-          onClick={() =>
-            setCurrentIndex(
-              (prevIndex) => (prevIndex - 1 + slides.length) % slides.length
-            )
-          }
-          data-carousel-prev
-        >
-          <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-            <svg
-              className="w-4 h-4 text-black dark:text-gray-800 rtl:rotate-180"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 6 10"
-            >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M5 1 1 5l4 4"
-              />
-            </svg>
-            <span className="sr-only">Previous</span>
-          </span>
-        </button>
-        <button
-          type="button"
-          className="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
-          onClick={() =>
-            setCurrentIndex((prevIndex) => (prevIndex + 1) % slides.length)
-          }
-          data-carousel-next
-        >
-          <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-            <svg
-              className="w-4 h-4 text-black dark:text-gray-800 rtl:rotate-180"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 6 10"
-            >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="m1 9 4-4-4-4"
-              />
-            </svg>
-            <span className="sr-only">Next</span>
-          </span>
-        </button>
+      <div className=" mt-14 h-56 sm:h-64 xl:h-80 2xl:h-96">
+        <Carousel slideInterval={2000}>
+          <img src={pc1} alt="..." />
+          <img src={pc2} alt="..." />
+          <img src={pc3} alt="..." />
+          <img src={pc4} alt="..." />
+        </Carousel>
       </div>
     </>
   );
